04a2fa797da1210d4aac8df26dd75086347c782f
Asif <asifjirayat@gmail.com>
2025-08-15 15:25:15 +0530

    Added validation and wired cancel button in NewProject component.

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,15 @@ export default function App() {
     });
   }
 
+  function handleCancelAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+      };
+    });
+  }
+
   function handleAddProject(projectData) {
     setProjectState((prevState) => {
       const newProject = {
@@ -36,7 +45,9 @@ export default function App() {
   let content;
 
   if (projectState.selectedProjectId === null) {
-    content = <NewProject onAdd={handleAddProject} />;
+    content = (
+      <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
+    );
   } else if (projectState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
---
+++ src/components/Modal.jsx
@@ -0,0 +1,31 @@
+import { createPortal } from "react-dom";
+import { useImperativeHandle, forwardRef, useRef } from "react";
+import Button from "./Button.jsx";
+
+const Modal = forwardRef(function Modal({ children, buttonCaption }, ref) {
+  const dialog = useRef();
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
+
+  return createPortal(
+    <dialog
+      ref={dialog}
+      className="backdrop:bg-stone-900/90 p-4 rounded-md shadow-md m-auto"
+    >
+      {children}
+      <form method="dialog">
+        <Button className="mt-4 text-right px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
+          {buttonCaption}
+        </Button>
+      </form>
+    </dialog>,
+    document.getElementById("modal-root")
+  );
+});
+
+export default Modal;
